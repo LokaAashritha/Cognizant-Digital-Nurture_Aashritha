7e95a9754fcde389915a2d37b4a83e94e3f3c6c7
Loka Aashritha <aashrithaloka22@gmail.com>
2026-06-03 13:16:43 +0530

    Add files via upload

---
+++ Loka Aashritha_8001491/Module_3/Exercise34_Modules/com.greetings/src/com/greetings/GreetingApplication.java
@@ -0,0 +1,11 @@
+package com.greetings;
+
+import com.utils.GreetingUtility;
+
+public class GreetingApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println(GreetingUtility.getMessage());
+    }
+}
